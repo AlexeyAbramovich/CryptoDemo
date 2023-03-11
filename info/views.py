2a570e60c17596480cb7cd58ca6aa8de7413ab2d
AlexAbra <alexgithub444356@gmail.com>
2023-03-11 11:53:38 +0300

    created working home page

---
+++ info/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+import requests
+import json
+
+
+# Create your views here.
+
+
+class HomePageView(ListView):
+    __path = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C7d'
+    template_name = "home_page.html"
+    context_object_name = "crypto_info"
+
+    def get_queryset(self):
+        response = requests.get(self.__path).json()
+        data = json.dumps(response)
+        data = json.loads(data)
+        info_list = []
+
+        # counter = 1
+        for inf in data:
+            info = {}
+            changes_for_1h = inf['price_change_percentage_1h_in_currency']
+            changes_for_24h = inf['price_change_percentage_24h']
+            changes_for_7d = inf['price_change_percentage_7d_in_currency']
+            changes_for_1h = changes_for_1h if changes_for_1h is not None else 0.0
+            changes_for_24h = changes_for_24h if changes_for_24h is not None else 0.0
+            changes_for_7d = changes_for_7d if changes_for_7d is not None else 0.0
+            info["position"] = f"{inf['market_cap_rank']}"
+            info["name"] = f" {inf['name']}"
+            info["current_price"] = f" {inf['current_price']}$"
+            info["changes_for_1h"] = round(changes_for_1h, 1)
+            info["changes_for_24h"] = round(changes_for_24h, 1)
+            info["changes_for_7d"] = round(changes_for_7d, 1)
+            info_list.append(info)
+            # info.append(f"{inf['market_cap_rank']}." + f" {inf['name']}" + f" {inf['current_price']}$" +
+            #             f" {round(changes_for_1h, 1)}%" + f" {round(changes_for_24h, 1)}%" +
+            #             f" {round(changes_for_7d, 1)}%")
+            # counter += 1
+        return info_list
