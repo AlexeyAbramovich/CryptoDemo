c1848cd913e88b5769cd5cdbe583fffd33734dbe
AlexAbra <alexgithub444356@gmail.com>
2023-03-14 08:35:48 +0300

    comlete page info 250 coins

--- info/views.py
+++ info/views.py
@@ -5,7 +5,7 @@ import json
 
 class HomePageView(ListView):
     __path = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C7d'
-    template_name = "home_page.html"
+    template_name = "info_250_coins.html"
     context_object_name = "crypto_info"
 
     def get_queryset(self):
@@ -24,7 +24,7 @@ class HomePageView(ListView):
             info["image"] = inf['image']
             info["name"] = inf['name']
             info["symbol"] = str(inf['symbol']).upper()
-            info["current_price"] = f" {inf['current_price']}$"
+            info["current_price"] = inf['current_price']
             info["changes_for_1h"] = round(changes_for_1h, 1)
             info["changes_for_24h"] = round(changes_for_24h, 1)
             info["changes_for_7d"] = round(changes_for_7d, 1)
